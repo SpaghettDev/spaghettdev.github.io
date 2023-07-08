928489b054d3c42c8d981b184209cc19e2f07b01
SpaghettDev <37266659+SpaghettDev@users.noreply.github.com>
2023-07-08 12:07:21 -0500

    trol 404

--- 404.js
+++ 404.js
@@ -5,4 +5,9 @@ if (getCookie(document, "bg") == null) {
 	setCookie(document, "bg", isBGModeToggled(window) ? "on" : "off");
 }
 
-document.getElementById("reason").outerText = `The requested resource doesn't exist. (${window.location.href.split("/").splice(3).join("")})`;
+if (!window.location.href.endsWith("404.html")) {
+    document.getElementById("reason").outerText = `The requested resource doesn't exist. (${window.location.href.split("/").splice(3).join("")})`;
+} else {
+    document.getElementById("reason").outerText = `The requested resource does exist.`;
+    document.getElementById("troll").hidden = false;
+}
