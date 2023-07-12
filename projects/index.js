187f1e362e215bf57f54d69a5c909bd5328fe46e
SpaghettDev <37266659+SpaghettDev@users.noreply.github.com>
2023-07-12 10:33:42 -0500

    fix stuff and add more projects

--- projects/index.js
+++ projects/index.js
@@ -1,46 +1,61 @@
-const createProjectBox = (project) => {
-	let node = document.getElementsByClassName("project-box-wrapper")[0].cloneNode(true);
-	let nodeContent = node.childNodes[1].childNodes[1].childNodes;
+const showErrorPopup =  () => {
+	document.getElementById("alert-button").style.display = "";
+	document.getElementById("alert-button").checked = false;
+	document.getElementsByClassName("alert-parent")[0].style.display = "block";
+}
+
+const getRepoData = async (projectLink) => {
+	return fetch(projectLink.replace("github.com", "api.github.com/repos"))
+		.then(res => res.json())
+		.then(res => res)
+		.catch(e => {});
+}
+
+const createProjectBox = async (project) => {
+	const node = document.getElementsByClassName("project-box-wrapper")[0].cloneNode(true);
+	const nodeContent = node.childNodes[1].childNodes[1].childNodes;
 
 	for (let i = 0; i < nodeContent.length; i++) {
-		for (let j = 0; j < nodeContent.length; j++) {
-			if (nodeContent[j].nodeName == "A") {
-				nodeContent[j].href = project["project_link"];
+		if (nodeContent[i].nodeName == "A") {
+			nodeContent[i].href = project["project_link"];
+			continue;
+		}
+
+		const nodeContentText = nodeContent[i].textContent;
+
+		if (nodeContentText) {
+			if (nodeContentText.includes("gh_")) {
+				const splitText = nodeContentText.split(/[\{\}]/);
+
+				nodeContent[i].childNodes[1].textContent = "...";
+				let fetchedValue = "...";
+				const response = await getRepoData(project["project_link"]);
+
+				if (!response || Object.keys(response).length === 0) {
+					showErrorPopup();
+				} else {
+					switch (splitText[1]) {
+						case "gh_size":
+							fetchedValue = `${parseInt(response[splitText[1].replace("gh_", "")]) / 1000} MBs`;
+							break;
+						default:
+							fetchedValue = response[splitText[1].replace("gh_", "")];
+					}
+				}
+
+				nodeContent[i].childNodes[1].textContent = fetchedValue;
 				continue;
 			}
 
-			let nodeContentText = nodeContent[j].outerText;
+			const splitText = nodeContentText.split(/[\{\}]/);
+			let newText = "";
 
-			if (nodeContentText) {
-	if (nodeContentText.includes("gh_")) {
-		let splitText = nodeContentText.split(/[\{\}]/);
-
-		// nodeContent[j].childNodes[1].textContent = "...";
-
-		fetch(project["project_link"].replace("github.com", "api.github.com/repos"))
-			.then(r => r.json())
-			.then(r => {
-				console.log(nodeContent[j].childNodes)
-				switch (splitText[1]) {
-					case "gh_size":
-						nodeContent[j].childNodes[1].textContent = `${parseInt(r[splitText[1].replace("gh_", "")]) / 1000} MBs`;
-						break;
-					default:
-						nodeContent[j].childNodes[1].textContent = r[splitText[1].replace("gh_", "")];
-				}
-			});
-		continue;
-	}
-
-				let splitText = nodeContentText.split(/[\{\}]/);
-				let newText = "";
-
-				for (let l = 0; l < splitText.length; l++) {
-					newText += project[splitText[l]] || splitText[l];
-				}
-
-				nodeContent[j].outerText = newText;
+			for (let l = 0; l < splitText.length; l++) {
+				newText += project[splitText[l]] || splitText[l];
 			}
+
+			if (nodeContent[i].childNodes.length > 1)
+				nodeContent[i].childNodes[1].textContent = newText;
 		}
 	}
 
@@ -56,53 +71,24 @@ if (getCookie(document, "bg") == null) {
 	setCookie(document, "bg", isBGModeToggled(window) ? "on" : "off");
 }
 
-let projectBox = document.getElementsByClassName("project-box-content");
 fetch("/api/projects/projects.json")
 	.then(r => r.json())
-	.then(projects => {
+	.then(async (projects) => {
 		for (let i = 0; i < projects["new"].length; i++) {
-			document.getElementsByClassName("projects-container")[0].appendChild(createProjectBox(projects["new"][i]));
-// 			for (let j = 0; j < projectBox.length; j++) {
-// 				let elemCh = projectBox[j].childNodes;
+			document.getElementsByClassName("projects-container")[0].appendChild(
+				await createProjectBox(projects["new"][i])
+			);
+		}
 
-// 				for (let k = 0; k < elemCh.length; k++) {
-// 					if (elemCh[k].nodeName == "A") {
-// 						elemCh[k].href = projects["new"][i]["project_link"];
-// 						continue;
-// 					}
-
-// 					let elemChText = elemCh[k].outerText;
-
-// 					if (elemChText) {
-// 						if (elemChText.includes("gh_")) {
-// 							let splitText = elemChText.split(/[\{\}]/);
-
-// 							elemCh[k].childNodes[1].textContent = "...";
-
-// 							fetch(projects["new"][i]["project_link"].replace("github.com", "api.github.com/repos"))
-// 								.then(r => r.json())
-// 								.then(r => {
-// 									switch (splitText[1]) {
-// 										case "gh_size":
-// 											elemCh[k].childNodes[1].textContent = parseInt(r[splitText[1].replace("gh_", "")]) / 1000}
-// 											break;
-// 										default:
-// 											elemCh[k].childNodes[1].textContent = r[splitText[1].replace("gh_", "")];
-// 									}
-// 								});
-// 							continue;
-// 						}
-
-// 						let splitText = elemChText.split(/[\{\}]/);
-// 						let newText = "";
-
-// 						for (let l = 0; l < splitText.length; l++) {
-// 							newText += projects["new"][i][splitText[l]] || splitText[l];
-// 						}
-
-// 						elemCh[k].outerText = newText;
-// 					}
-// 				}
-// 			}
+		for (let i = 0; i < projects["old"].length; i++) {
+			document.getElementsByClassName("projects-container")[1].appendChild(
+				await createProjectBox(projects["old"][i])
+			);
 		}
 	});
+
+
+document.getElementById("alert-button").onclick = () => {
+	document.getElementById("alert-button").style.display = "none";
+	document.getElementById("alert-svg").style.display = "none";
+};
